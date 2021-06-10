174860ed93798d5929c2a38707f68d28b4c142ec
obedkristiaji <kristiajio@gmail.com>
2021-06-10 12:17:41 +0700

    fix app structure and update about us

--- app/src/main/java/id/ac/unpar/informatika/dadoo/view/HomeFragment.java
+++ app/src/main/java/id/ac/unpar/informatika/dadoo/view/HomeFragment.java
@@ -1,6 +1,10 @@
 package id.ac.unpar.informatika.dadoo.view;
 
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
@@ -19,21 +23,35 @@ import id.ac.unpar.informatika.dadoo.presenter.IMainPresenter;
 import id.ac.unpar.informatika.dadoo.thread.RandomThread;
 import id.ac.unpar.informatika.dadoo.thread.ThreadHandler;
 
-public class HomeFragment extends Fragment implements View.OnClickListener {
+public class HomeFragment extends Fragment implements View.OnClickListener, SensorEventListener {
     private FragmentHomeBinding binding;
     private IMainActivity listener;
     private IMainPresenter presenter;
     private ThreadHandler handler;
     private RandomThread thread;
-    private ImageView imageView;
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+    private float[] accelerometerReading = new float[3];
+    private int shakeThreshold = 800;
+    private long lastUpdate = 0;
+    private  Vibrator v;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = FragmentHomeBinding.inflate(inflater, container, false).getRoot();
         this.binding = FragmentHomeBinding.bind(view);
 
+        this.mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        this.accelerometer = this.mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        this.handler = new ThreadHandler(this.presenter);
+
+        this.v = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+
         this.binding.btnLempar.setOnClickListener(this);
         this.binding.btnTentang.setOnClickListener(this);
+
         return view;
     }
 
@@ -48,13 +66,53 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-    public static HomeFragment newInstance(IMainPresenter presenter, ThreadHandler handler) {
+    public static HomeFragment newInstance(IMainPresenter presenter) {
         HomeFragment fragment = new HomeFragment();
-        fragment.handler = handler;
         fragment.presenter = presenter;
         return fragment;
     }
-      
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(this.accelerometer != null) {
+            this.mSensorManager.registerListener(this, this.accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            if (curTime - lastUpdate > 100) {
+                long diffTime = curTime - lastUpdate;
+                lastUpdate = curTime;
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                float speed = Math.abs(x + y + z - accelerometerReading[0] - accelerometerReading[1] - accelerometerReading[2]) / diffTime * 10000;
+                if (speed > this.shakeThreshold) {
+                    this.vibrate();
+                }
+                accelerometerReading[0] = x;
+                accelerometerReading[1] = y;
+                accelerometerReading[2] = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //
+    }
+
+
     public void setGambar(int num){
         this.binding.tvHome.setVisibility(View.GONE);
         if(num==1){
@@ -77,17 +135,20 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         this.thread.start();
     }
 
+    public void vibrate() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(200);
+        }
+        this.startThread();
+    }
+
     @Override
     public void onClick(View v) {
         if (v == this.binding.btnLempar) {
-            Vibrator vib = (Vibrator)  getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                vib.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
-            } else {
-                //deprecated in API 26
-                vib.vibrate(200);
-            }
-            this.startThread();
+            this.vibrate();
         } else if (v == this.binding.btnTentang) {
             this.listener.changePage("About");
         }
